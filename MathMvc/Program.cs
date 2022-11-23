78b50bed094e5afdb495099645eb85199252124d
gustavo.voltolini <gustavo.voltolini@ambevtech.com.br>
2022-11-23 01:05:51 -0300

    Changing login route

--- MathMvc/Program.cs
+++ MathMvc/Program.cs
@@ -44,7 +44,7 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.Cookie.HttpOnly = true;
     options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
-    options.LoginPath = "/Identity/Account/Login";
+    options.LoginPath = "/login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
     options.SlidingExpiration = true;
 });
